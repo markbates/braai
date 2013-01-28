b3121cd6a79d525754f4c37ec80a4275b6aa2070
Nick Plante <nap@zerosum.org>
2013-01-28 15:34:44 -0500

    reorganize handlers

---
+++ lib/braai/handlers.rb
@@ -0,0 +1,67 @@
+module Braai::Handlers
+  class Base
+    attr_accessor :template, :key, :matches
+
+    def initialize(template, key, matches)
+      @template = template
+      @key = key
+      @matches = matches
+    end
+
+    class << self
+
+      def call(template, key, matches)
+        handler = self.new(template, key, matches)
+        handler.safe_perform
+      end
+
+    end
+
+    def safe_perform
+      begin
+        perform
+      rescue => e
+        rescue_from_error(e)
+      end
+    end
+
+    def perform
+      key
+    end
+
+    def rescue_from_error(e)
+      nil
+    end
+  end
+
+  class IterationHandler < Base
+    def perform
+      res = []
+      template.attributes[matches[2]].each do |val|
+        res << Braai::Context.new(matches[3], template.matchers, template.attributes.merge(matches[1] => val)).render
+      end
+      res.join("\n")
+    end
+  end
+
+  class DefaultHandler < Base
+    def perform
+      chain = matches[1].split('.')
+      value = template.attributes[chain.shift]
+      return nil unless value
+
+      chain.each do |a|
+        if value.respond_to?(a.to_sym)
+          value = value.send(a)
+        elsif value.is_a?(Hash)
+          value = value[a.to_sym] || value[a.to_s]
+        else
+          return nil
+        end
+      end
+
+      value
+    end
+  end
+
+end
